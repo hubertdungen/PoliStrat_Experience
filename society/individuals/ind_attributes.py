2cd7845da042c87fc3d6623e706d323f7c1b19b0
Hubert Dungen <hubertdungen@gmail.com>
2024-05-17 02:27:44 +0100

    v.0.0.3
    - Major ind_attributes.py update, including a lot of new DNA vars

--- society/individuals/ind_attributes.py
+++ society/individuals/ind_attributes.py
@@ -56,109 +56,114 @@ from enum import Enum
 @dataclass
 class DNA:
     # Species Classifications
-    species: str                                    # Species name
-    species_phylum: str                             # Phylum name (Animal, Plant, Fungi, etc) 
-    species_class: str                              # Class name (Mammal, Bird, Fish, etc)
-    species_family: str                             # Family name
-    # Species traits
-    species_diet: DietType                          # Diet type
-    species_mortality: MortalityType                # Mortality type
-    species_traits: Dict[str, Tuple[int, int]]      # All species traits combined
-    # Fisionomy
-    species_height: Tuple[int, int]                     # Height in meters
-    species_weight: Tuple[int, int]                     # Weight in kilograms
+    species: str                                        # Species name
+    species_phylum: str                                 # Phylum name (Animal, Plant, Fungi, etc) 
+    species_class: str                                  # Class name (Mammal, Bird, Fish, etc)
+    species_family: str                                 # Family name
     # Reproduction
     species_reproduction: ReproductionType              # Sexual or Asexual
     species_reproduction_type: ReproductionMode         # Viviparous, Oviparous, Ovoviviparous, Imortal (if asexual)
     species_reproduction_process: ReproductionProcess   # Autogamy, Parthenogenesis, Mitosis, Meiosis, Allogamy, Anisogamy, Isogamy
-    species_maturity: int                               # years / *maturity for reproduction*
-    species_fertility: int                              # probability of reproduction
-    species_gestation: int                              # months
-    # Personality
-    personalities: Dict[str, float]                 # Personality distribution with probabilities
+    species_reproduction_fertility: ReproductionFertility# Maturity, Fertility, Gestation
+    # Species traits
+    species_diet: DietType                              # Diet type
+    species_mortality: MortalityType                    # Mortality type
+    species_genetics: Genetics                          # Genetics attributes
+    species_fisionomy: Fisionomy                        # Fisionomy attributes 
+    species_senses: Senses                              # Senses attributes
+    # Mental
+    species_mental: MentalBehavior                      # Mental attributes and social behavior
+    
 
 
 class MortalityType(Enum):
-    MORTAL = "Mortal"                               # Pros: Can evolve, Cons: Can die             
-    IMMORTAL = "Immortal"                           # Pros: Can't die, Cons: Can't evolve
+    MORTAL = "Mortal"                                   # Pros: Can evolve, Cons: Can die             
+    IMMORTAL = "Immortal"                               # Pros: Can't die, Cons: Can't evolve
 
 
 class ReproductionType(Enum):
-    SEXUAL = "Sexual"                               # Sexual reproduction   / For example: Humans, Animals, etc... | Pros: Genetic diversity, Cons: Need to find a mate
-    ASEXUAL = "Asexual"                             # Asexual reproduction / For example: Bacteria, Fungi, etc...  | Pros: No need to find a mate, Cons: No genetic diversity
+    SEXUAL = "Sexual"                                   # Sexual reproduction   / For example: Humans, Animals, etc... | Pros: Genetic diversity, Cons: Need to find a mate
+    ASEXUAL = "Asexual"                                 # Asexual reproduction / For example: Bacteria, Fungi, etc...  | Pros: No need to find a mate, Cons: No genetic diversity
 
 
 class ReproductionMode(Enum):
-    VIVIPAROUS = "Viviparous"                       # Live birth
-    OVIPAROUS = "Oviparous"                         # Egg laying
-    OVOVIVIPAROUS = "Ovoviviparous"                 # Egg laying and live birth
+    VIVIPAROUS = "Viviparous"                           # Live birth
+    OVIPAROUS = "Oviparous"                             # Egg laying
+    OVOVIVIPAROUS = "Ovoviviparous"                     # Egg laying and live birth
 
 
 class ReproductionProcess(Enum):
-    AUTOGAMY = "Autogamy"                           # Self-fertilization
-    PARTHENOGENESIS = "Parthenogenesis"             # Asexual reproduction from an unfertilized egg
-    MITOSIS = "Mitosis"                             # Asexual reproduction by cell division
-    BUDDING = "Budding"                             # Asexual reproduction by budding
-    SPORULATION = "Sporulation"                     # Asexual reproduction by spores
-    MEIOSIS = "Meiosis"                             # Sexual reproduction by cell division
-    ALLOGAMY = "Allogamy"                           # Cross-fertilization
-    ANISOGAMY = "Anisogamy"                         # Sexual reproduction by fusion of gametes of different sizes / For example: Humans, Animals, etc...
-    ISOGAMY = "Isogamy"                             # Sexual reproduction by fusion of gametes of similar sizes / For example: Fungi, Algae, etc...
+    AUTOGAMY = "Autogamy"                               # Self-fertilization
+    PARTHENOGENESIS = "Parthenogenesis"                 # Asexual reproduction from an unfertilized egg
+    MITOSIS = "Mitosis"                                 # Asexual reproduction by cell division
+    BUDDING = "Budding"                                 # Asexual reproduction by budding
+    SPORULATION = "Sporulation"                         # Asexual reproduction by spores
+    MEIOSIS = "Meiosis"                                 # Sexual reproduction by cell division
+    ALLOGAMY = "Allogamy"                               # Cross-fertilization
+    ANISOGAMY = "Anisogamy"                             # Sexual reproduction by fusion of gametes of different sizes / For example: Humans, Animals, etc...
+    ISOGAMY = "Isogamy"                                 # Sexual reproduction by fusion of gametes of similar sizes / For example: Fungi, Algae, etc...
+
+
+class ReproductionFertility(Enum):
+    MATURITY = "Maturity"                               # Maturity for reproduction | in years
+    FERTILITY = "Fertility"                             # Probability of reproduction | in %
+    GESTATION = "Gestation"                             # Gestation period | in months
 
 
 class Genetics(Enum):
-    LIFESPAN = "Lifespan"                           # How long the individual can live
-    RESISTANCE = "Resistance"                       # Resistance to diseases, injuries, etc...
-    DIMORPHISM = "Dimorphism"                       # Sexual dimorphism, physical differences
-    INTELLIGENCE = "Intelligence"                   # Learning, problem solving, etc...
-    CRIATIVITY = "Creativity"                       # Creating, inventing, etc...
-    EVOLUTION = "Evolution"                         # Genetic evolution and adaptation, velocity of evolution
-    DIFFERENTIATION = "Differentiation"             # Genetic differentiation between individuals of the same species
+    LIFESPAN = "Lifespan"                               # How long the individual can live
+    DIMORPHISM = "Dimorphism"                           # Sexual dimorphism, physical differences
+    INTELLIGENCE = "Intelligence"                       # Learning, problem solving, etc...
+    CRIATIVITY = "Creativity"                           # Creating, inventing, etc...
+    EVOLUTION = "Evolution"                             # Genetic evolution and adaptation, velocity of evolution
+    DIFFERENTIATION = "Differentiation"                 # Genetic differentiation between individuals of the same species
 
 
 class DietType(Enum):
-    CARNIVORE = "Carnivore"                         # Meat eater
-    HERBIVORE = "Herbivore"                         # Plant eater
-    OMNIVORE = "Omnivore"                           # Meat and plant eater
-    DETRIVORE = "Detrivore"                         # Eats decomposing plants and animal parts as well as feces
-    INSECTIVORE = "Insectivore"                     # Eats insects
-    FRUGIVORE = "Frugivore"                         # Eats fruits
-    GRANIVORE = "Granivore"                         # Eats seeds
-    NECTARIVORE = "Nectarivore"                     # Eats nectar
-    PLANKTIVORE = "Planktivore"                     # Eats plankton
-    PISCIVORE = "Piscivore"                         # Eats fish
-    MYCOVORE = "Mycovore"                           # Eats fungi
-    XYLIVORE = "Xylivore"                           # Eats wood
-    LITHOVORE = "Lithovore"                         # Eats rocks
+    CARNIVORE = "Carnivore"                             # Meat eater
+    HERBIVORE = "Herbivore"                             # Plant eater
+    OMNIVORE = "Omnivore"                               # Meat and plant eater
+    DETRIVORE = "Detrivore"                             # Eats decomposing plants and animal parts as well as feces
+    INSECTIVORE = "Insectivore"                         # Eats insects
+    FRUGIVORE = "Frugivore"                             # Eats fruits
+    GRANIVORE = "Granivore"                             # Eats seeds
+    NECTARIVORE = "Nectarivore"                         # Eats nectar
+    PLANKTIVORE = "Planktivore"                         # Eats plankton
+    PISCIVORE = "Piscivore"                             # Eats fish
+    MYCOVORE = "Mycovore"                               # Eats fungi
+    XYLIVORE = "Xylivore"                               # Eats wood
+    LITHOVORE = "Lithovore"                             # Eats rocks
 
 
 class Fisionomy(Enum):
-    HEIGHT = "height"                           # height in meters
-    WEIGHT = "weight"                           # weight in kilograms
-    STRENGTH = "strength"                       # fighting, lifting, etc...
-    SPEED = "speed"                             # running, fighting, etc...
-    ENDURANCE = "endurance"                     # Stamina, this will be used for running, fighting, etc...            
-    DEXTERITY = "dexterity"                     # Agility, this will be used for crafting, building, moving eyes, hands, etc...
-    SPEECH = "speech"                           # communication, persuasion, etc...
-    SOCIAL = "social"                           # social interactions, empathy, etc...
-    VITALITY = "vitality"                       # health, wellness, etc...
+    HEIGHT = "height"                                   # height in meters
+    WEIGHT = "weight"                                   # weight in kilograms
+    STRENGTH = "strength"                               # fighting, lifting, etc...
+    SPEED = "speed"                                     # running, fighting, etc...
+    ENDURANCE = "endurance"                             # Stamina, this will be used for running, fighting, etc...            
+    DEXTERITY = "dexterity"                             # Agility, this will be used for crafting, building, moving eyes, hands, etc...
+    SPEECH = "speech"                                   # communication, persuasion, etc...
+    APPAREANCE = "appareance"                           # beauty, ugliness, etc...
+    VITALITY = "vitality"                               # health, wellness, etc...
+    RESISTANCE = "Resistance"                           # Resistance to diseases, injuries, etc...
+    
+
+class Senses(Enum):
+    VISION = "vision"                                   # sight, perception, etc...
+    HEARING = "hearing"                                 # sound, perception, etc...
+    SMELL = "smell"                                     # smell, perception, etc...
+    TASTE = "taste"                                     # taste, perception, etc...
+    TOUCH = "touch"                                     # touch, perception, etc...
 
 
-class Sense(Enum):
-    VISION = "vision"                           # sight, perception, etc...
-    HEARING = "hearing"                         # sound, perception, etc...
-    SMELL = "smell"                             # smell, perception, etc...
-    TASTE = "taste"                             # taste, perception, etc...
-    TOUCH = "touch"                             # touch, perception, etc...
-
-
-class Behavior(Enum):
-    EXTROVERSION = "Extroversion"               # Outgoing, assertive, active, talkative
-    OPENNESS = "Openness"                       # Imaginative, curious, creative, open-minded
-    AGREEABLENESS = "Agreeableness"             # Kind, affectionate, trusting, cooperative
-    CONSCIENTIOUSNESS = "Conscientiousness"     # Organized, responsible, reliable, hard-working
-    NEUROTICISM = "Neuroticism"                 # Anxious, sensitive, nervous, moody, emotional
-    AGGRESSION = "Aggression"                   # Emotional responses, tolerance, fighting, etc...
+class MentalBehavior(Enum):
+    EXTROVERSION = "Extroversion"                       # Outgoing, assertive, active, talkative
+    OPENNESS = "Openness"                               # Imaginative, curious, creative, open-minded
+    AGREEABLENESS = "Agreeableness"                     # Kind, affectionate, trusting, cooperative
+    CONSCIENTIOUSNESS = "Conscientiousness"             # Organized, responsible, reliable, hard-working
+    NEUROTICISM = "Neuroticism"                         # Anxious, sensitive, nervous, moody, emotional
+    AGGRESSION = "Aggression"                           # Impulsiveness, emotional responses, tolerance, fighting, etc...
+    SOCIAL = "Social"                                   # social interactions, empathy, etc...
 ##-----------------------------------------------------------------------##
 
 
@@ -168,22 +173,46 @@ class Behavior(Enum):
 @dataclass
 class Personality:
     # Personality Traits
-    extroversion: float                         # Outgoing, assertive, active, talkative
-    openness: float                             # Imaginative, curious, creative, open-minded
-    agreeableness: float                        # Kind, affectionate, trusting, cooperative
-    conscientiousness: float                    # Organized, responsible, reliable, hard-working
-    neuroticism: float                          # Anxious, sensitive, nervous, moody, emotional
-    aggression: float                           # Emotional responses, tolerance, fighting, etc...
+    personality_mental: MentalBehavior                  # Mental attributes and social behavior                                 
     # Myers-Briggs
-    myerbrigs: Dict[str, float]                 # Myers-Briggs personality distribution with probabilities
+    myerbrigs_name: MyerBrigs                           # Setting the personality type                    
 
 class MyerBrigs(Enum):
-    INTROVERSION = "Introversion"               # Reserved, reflective, quiet, thoughtful       | Letters: "I" or "E"
-    INTUITION = "Intuition"                     # Imaginative, innovative, creative, original   | Letters: "N" or "S"
-    THINKING = "Thinking"                       # Logical, objective, rational, analytical      | Letters: "T" or "F"
-    JUDGING = "Judging"                         # Decisive, firm, organized, controlling        | Letters: "J" or "P"
-    TURBULENCE = "Turbulence"                   # Sensitive, emotional, reactive, unstable      | Letters: "T" or "A"
-    MYERBRIGS_NAME = "MYERBRIGS_NAME"           # Name of the personality type | Can be: "Architect", "Logician", "Commander", "Debater", "Advocate", "Mediator", "Protagonist", "Campaigner", "Logistician", "Defender", "Executive", "Consul", "Virtuoso", "Adventurer", "Entrepreneur", "Entertainer"
-                                                # With letters being: "INTJ", "INTP", "ENTJ", "ENTP", "INFJ", "INFP", "ENFJ", "ENFP", "ISTJ", "ISFJ", "ESTJ", "ESFJ", "ISTP", "ISFP", "ESTP", "ESFP"
+    INTROVERSION = "Introversion"                       # Reserved, reflective, quiet, thoughtful       | Letters: "I" or "E" | Meaning: "I" = Introversion, "E" = Extroversion
+    INTUITION = "Intuition"                             # Imaginative, innovative, creative, original   | Letters: "N" or "S" | Meaning: "N" = Intuition, "S" = Sensing
+    THINKING = "Thinking"                               # Logical, objective, rational, analytical      | Letters: "T" or "F" | Meaning: "T" = Thinking, "F" = Feeling
+    JUDGING = "Judging"                                 # Decisive, firm, organized, controlling        | Letters: "J" or "P" | Meaning: "J" = Judging, "P" = Perceiving
+    TURBULENCE = "Turbulence"                           # Sensitive, emotional, reactive, unstable      | Letters: "T" or "A" | Meaning: "T" = Turbulence, "A" = Assertive
+    MYERBRIGS_NAME = "MYERBRIGS_NAME"                   # Name of the personality type | Can be: "Architect", "Logician", "Commander", "Debater", "Advocate", "Mediator", "Protagonist", "Campaigner", "Logistician", "Defender", "Executive", "Consul", "Virtuoso", "Adventurer", "Entrepreneur", "Entertainer"
+                                                        # With letters being: "INTJ", "INTP", "ENTJ", "ENTP", "INFJ", "INFP", "ENFJ", "ENFP", "ISTJ", "ISFJ", "ESTJ", "ESFJ", "ISTP", "ISFP", "ESTP", "ESFP"
+##-----------------------------------------------------------------------##
+
+
+
+## Mood Class
+# ---------------------------------------------------------------------- #
+@dataclass
+class Mood:
+    # Mood Traits
+    mood_type: MoodType                                # Mood type
+
+class MoodType(Enum):
+    HAPPINESS = "Happiness"                             # Happiness level
+    SADNESS = "Sadness"                                 # Sadness level
+    ANGER = "Anger"                                     # Anger level
+    FEAR = "Fear"                                       # Fear level
+    DISGUST = "Disgust"                                 # Disgust level
+    SURPRISE = "Surprise"                               # Surprise level
+    ALERTNESS = "Alertness"                             # Alertness level
+    SAFETY = "Safety"                                   # Safety feeling level
+    SLEEPINESS = "Sleepiness"                           # Sleepiness level
+    TIREDNESS = "Tiredness"                             # Tiredness level
+    FOCUS = "Focus"                                     # Focus level
+    CURIOSITY = "Curiosity"                             # Curiosity level
+    PERCEPTION = "Perception"                           # Perception level
+##-----------------------------------------------------------------------##
+
+
+
 
 
